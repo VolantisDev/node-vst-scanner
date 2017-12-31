74b6334f459587185329618606913ec2ba87b8da
Ben McClure <ben.mcclure@gmail.com>
2017-12-30 22:14:48 -0600

    Fix vst-scanner and update test file

--- test.js
+++ test.js
@@ -1,21 +1,10 @@
-const Promise = require('bluebird')
 const vstScanner = require('./lib/vst-scanner')
-const vstReader = require('./lib/vst-reader')
 
 var dirs = [
   'E:\\Music\\VST Plugins'
 ]
 
-vstScanner(dirs, 'vst3')
+vstScanner(dirs, false, true)
   .then(function (results) {
-
-    var vsts = {}
-    Object.keys(results).forEach(function (key) {
-      vsts[key] = vstReader(results[key].path)
-    })
-
-    return Promise.props(vsts)
-      .then(function (vsts) {
-        console.log(vsts)
-      })
+    console.log(results)
   })
